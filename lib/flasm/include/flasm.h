0e0f26e1d2e944784f4951752e7842aea31df1d6
chathamabate <chathamabate@gmail.com>
2023-11-02 17:52:47 -0400

    Starting code organization

---
+++ lib/flasm/include/flasm.h
@@ -0,0 +1,24 @@
+
+
+namespace flasm {
+
+    // The goal is to design this in such a way that addition of newly 
+    // supported instruction types is easy!
+    // The construction should not be specific to any single set of 
+    // instructions.
+
+    class Instruction {
+    };
+
+    // Should there be a different class for each instruciton type??
+    // Maybe....
+    class MoveInstruction {
+           
+
+    };
+
+    class Operand {
+        // Operands have many different types.
+        // This will be hard to figure out.
+    };
+}
