f018c8d7d617924f23bfba42946b1e92f4ba3422
chathamabate <chathamabate@gmail.com>
2023-11-02 22:20:09 -0400

    My head hurts

--- lib/flasm/include/flasm.h
+++ lib/flasm/include/flasm.h
@@ -2,6 +2,15 @@
 
 namespace flasm {
 
+    // My first IR should mix assembly with real code?
+    // Honestly, it would be nice if this were always the case...
+    // Let's load the entire thing into memory.
+    // It will be slower, but more flexibly for future choices.
+
+    // Should I have abstraction for instructions?
+    // Or should this be the final stop of the compiler?
+    // I would say that we provide abstraction?
+
     // The goal is to design this in such a way that addition of newly 
     // supported instruction types is easy!
     // The construction should not be specific to any single set of 
